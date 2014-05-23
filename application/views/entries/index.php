2f7e33ff2fafa50c6dcf7c3f7b9eb53cc2dfc448
Steve Upton <supton@overtsoftware.com>
2014-05-23 18:01:11 +0100

    Mobile responsiveness fixes

--- application/views/entries/index.php
+++ application/views/entries/index.php
@@ -6,21 +6,26 @@
 		var clip = new ZeroClipboard($(".clip_button"));
 	});
 </script>
-<?php
-foreach ($entries as $entry) {
-	$output = '<div class="entry"><a href="' . site_url('/entries/edit/' . $entry->id) . '"><i class="fa fa-pencil-square-o"></i></a>' .
-			'<span class="entry-title">' . $entry->title . '</span> ';
-	
-	if (!empty($entry->tags)) {
-		$tags = explode(',', $entry->tags);
-		foreach ($tags as $tag) {
-			$output .= '<a class="label label-default label-' . $tag .'" href="' . site_url('/entries/index/'. $tag) . '">' .$tag . '</a>';
-		}
-	}
-	$output .= '<div><pre class="prettyprint lang-bash" id="code_' . $entry->id . '">' . html_escape($entry->code) . '</pre>';
-	$output .= '<button class="btn btn-default clip_button" data-clipboard-target="code_' . $entry->id . '"><i class="fa fa-files-o"></i></button></div>';
-	if (!empty($entry->description)) {
-		$output .= '<div class="entry-desc">' . html_escape($entry->description) . '</div>';
-	}
-	echo $output . '</div>';
-}
+<?php foreach ($entries as $entry) { ?>
+	<div class="entry">
+		<a href="<?php echo site_url('/entries/edit/' . $entry->id)  ?>"><i class="fa fa-pencil-square-o"></i></a>
+		<span class="entry-title"><?php echo $entry->title ?></span>
+		<?php 
+			if (!empty($entry->tags)) {
+				$tags = explode(',', $entry->tags);
+				foreach ($tags as $tag) {
+					echo '<a class="label label-default label-' . $tag .'" href="' . site_url('/entries/index/'. $tag) . '">' .$tag . '</a>';
+				}
+			}
+		?>
+		<div class="entry-content">
+			<pre class="prettyprint lang-bash" id="code_<?php echo $entry->id ?>"><?php echo html_escape($entry->code) ?></pre>
+			<?php 
+			if (!empty($entry->description)) {
+				echo '<div class="entry-desc">' . html_escape($entry->description) . '</div>';
+			}
+			?>
+		</div>
+		<button class="btn btn-default clip_button" data-clipboard-target="code_<?php echo $entry->id ?>"><i class="fa fa-files-o"></i></button>
+	</div>
+<?php  } ?>
